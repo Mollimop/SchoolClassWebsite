852a32c7b4f345f6aae99fec3d96dab776b0bad1
TomBom4 <thomas.kirz@gmail.com>
2016-01-29 18:24:40 +0100

    Update Password.java and User.java and improve redirecting Remove unnecessary CSS

--- src/main/java/io/github/tombom4/userManagement/Password.java
+++ src/main/java/io/github/tombom4/userManagement/Password.java
@@ -7,10 +7,16 @@ public class Password {
     private String encryptedPassword = "";
 
     public Password(String password){
-        cryptPassword(password);
+        encryptPassword(password);
     }
 
-    public void cryptPassword(String psw){
+    public static Password byEncryptedPassword(String password) {
+        Password psw = new Password("");
+        psw.setEncryptedPassword(password);
+        return psw;
+    }
+
+    public void encryptPassword(String psw){
         char[] passwordChars = psw.toCharArray();
         int[] passwordCode = new int[passwordChars.length];
         long[] passwordCodeEncrypted = new long[passwordChars.length];
@@ -23,5 +29,9 @@ public class Password {
 
     public String toString() {
         return encryptedPassword;
+    }
+
+    private void setEncryptedPassword(String password) {
+        encryptedPassword = password;
     }
 }
