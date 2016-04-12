c39bb6a29f44f8a37103d051c85f1bb5102d5e23
Vogte999 <vogt999@gmail.com>
2016-04-12 21:18:11 +0200

    Add main controller and set .sass as default for sass files

---
+++ SchoolClassWebsite/app/controllers/main_controller.rb
@@ -0,0 +1,10 @@
+class MainController < ApplicationController
+  def home
+  end
+
+  def newEvent
+  end
+
+  def newHomework
+  end
+end
--- SchoolClassWebsite/config/routes.rb
+++ SchoolClassWebsite/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'main/home'
+  get 'main/newEvent'
+  get 'main/newHomework'
+
+  root :to => "main#home"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
