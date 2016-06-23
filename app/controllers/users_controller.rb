6ca2a82fedf9e2f47a690a11d58f302ea0449865
Vogte <Vogt999@gmail.com>
2016-06-23 21:05:47 +0200

    Add some new account functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @users.each do |user|
       if((user["name"] == @user["name"]) && (user["password"]== @user["password"]))
         login = true
+        session[:userid] = user.id
         break
       end
     end
@@ -23,7 +24,43 @@ class UsersController < ApplicationController
     redirect_to '/login'
   end
 
-  private def user_params()
-    params.require(:user).permit(:name, :password)
+  def show
+    @threads = Array.new()
+    @posts = Array.new()
+    @user = User.find(session[:userid])
+
+    Post.all.each do |post|
+      if post.user == @user
+        @posts << post
+      end
+    end
+
+    ForumThread.all.each do |thread|
+      if thread.user == @user
+        @threads << thread
+      end
+    end
+
   end
+
+  def update
+    @user = User.find(session[:userid])
+    suc = @user.update(password: user_paramsp()["password"])
+    session[:user] = User.find(session[:userid])
+    if suc
+      session[:updatepsw] = 0
+    else
+      session[:updatepsw] = 1
+    end
+    redirect_to '/users/show'
+  end
+
+
+  private
+    def user_params()
+      params.require(:user).permit(:name, :password)
+    end
+    def user_paramsp()
+      params.require(:user).permit(:password)
+    end
 end
